6aef8dc25775a5ca6852eaa13601d631b468c292
Sarvasv Kulpati <sarvasvkulpati@gmail.com>
2019-09-15 23:43:15 +0800

    removed all boilerplate, added basic components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { Header } from './components/layout/Header'
+import { Content } from './components/layout/Content';
 
 
 export const App = () => (
     <div className="App">
-      <header className="App-header">
-        <p> hello world</p>
-      </header>
+      <Header/>
+      <Content/>
     </div>
 );
 
